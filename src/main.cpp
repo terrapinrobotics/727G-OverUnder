bd13ce90f7671ff960521b756badbedfd27088a3
terrapinrobotics <terrapinrobotics@gmail.com>
2023-09-06 15:42:01 -0400

    intake

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 auto chassis =
 	reauto::ChassisBuilder<>()
-		.motors({-20, 19, 18}, {-16, 15, 13}, pros::Motor_Gears::blue)
+		.motors({20, -19, -18}, {16, 15, -13}, pros::Motor_Gears::blue)
 		.controller(master)
 		.build();
+
+pros::Motor intake(11);
 
 void initialize() {}
 
@@ -23,6 +25,19 @@ void opcontrol() {
 
 	while (true) {
 		chassis->tank();
+
+		if (master.get_digital(DIGITAL_R1)) {
+			intake.move_voltage(-12000);
+		}
+
+		else if (master.get_digital(DIGITAL_R2)) {
+			intake.move_voltage(12000);
+		}
+
+		else {
+			intake.brake();
+		}
+
 		pros::delay(10);
 	}
 }
