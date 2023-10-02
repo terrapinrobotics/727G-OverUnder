689230ab81ba3233935e68ed99e4034d50588614
terrapinrobotics <terrapinrobotics@gmail.com>
2023-10-02 15:58:52 -0400

    add MP

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,13 @@ auto chassis =
 	reauto::ChassisBuilder<>()
 		.motors({14, -19, -18}, {16, 15, -13}, pros::Motor_Gears::blue)
 		.controller(master)
+		.setTrackWidth(10.5_in)
+		.imu(21)
 		.build();
+
+reauto::TrapezoidalProfileConstants c = { 95, 1.34, 28, 0.5 };
+
+auto profile = reauto::TrapezoidalProfile(chassis, c, nullptr);
 
 // the catapult is free spinning, so no need to add any functionality to reauto.
 pros::Motor cata(12);
@@ -18,13 +24,18 @@ pros::adi::Pneumatics walls('A', false);
 pros::adi::Pneumatics doinker('B', false);
 pros::adi::Pneumatics climb('E', false);
 
+
+
 void initialize() {}
 
 void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {}
+void autonomous() {
+	profile.compute(48_in);
+	profile.followLinear();
+}
 
 void opcontrol() {
 	chassis->setDriveExponent(3);
