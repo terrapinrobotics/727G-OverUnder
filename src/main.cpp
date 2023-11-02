8ff8efafd0b1ab100901b52f9d3f45360e5cd998
SprintKeyz <davislshapiro@gmail.com>
2023-11-01 23:29:35 -0400

    :sparkles: Finish base autonomous skills path

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "pros/misc.h"
 #include "pros/misc.hpp"
 #include "reauto/api.hpp"
 #include "reauto/motion/purepursuit/PurePursuit.hpp"
@@ -26,6 +27,9 @@ pros::Motor intake(11);
 pros::adi::Pneumatics walls('A', false);
 pros::adi::Pneumatics doinker('B', false);
 pros::adi::Pneumatics climb('E', false);
+
+#define LAT_MOVE 12
+#define ANG_MOVE 45
 
 // set up PID controller for lateral movement
 std::vector<IPIDConstants> latConstants = {
@@ -62,7 +66,7 @@ PIDExits angExits = {
 
 auto latPID = std::make_shared<reauto::controller::PIDController>(latConstants, latExits, 0, 5);
 auto angPID = std::make_shared<reauto::controller::PIDController>(angConstants, angExits, 5);
-auto controller = std::make_shared<reauto::MotionController>(chassis, latPID.get(), angPID.get(), 5);
+auto controller = std::make_shared<reauto::MotionController>(chassis, latPID.get(), angPID.get());
 
 auto purePursuit = std::make_shared<reauto::motion::PurePursuit>(chassis.get());
 
@@ -74,16 +78,68 @@ void disabled() {}
 
 void competition_initialize() {}
 
-void shootLoop() {
+void shootLoop(int time_ms) {
 	// shoot for ~45 seconds
 	cata.move_voltage(10800);
-	pros::delay(45000);
+	pros::delay(time_ms);
 	cata.move_voltage(0);
 }
 
 void autonomous() {
-	//shootLoop(); // shoot all our triballs
-	controller->turn(50_deg, 127, false, 750);
+	//purePursuit->follow("/usd/path.txt", 20000, 10);
+
+	// PID testing
+	//controller->turn(90_deg);
+
+	controller->turn(-32_deg);
+
+	//shootLoop(31000);
+
+	// delay for safety
+	//pros::delay(1500);
+
+	controller->drive({0, 82}, 80_pct, false, 1950);
+
+	// turn around the corner
+	controller->drive({-20, 82}, 100_pct, true, 1200);
+	controller->drive({-22, 56}, 100_pct, true, 900);
+	controller->drive({-22, 56}, 80_pct, true, 900);
+	
+	controller->drive({-63, 56}, 80_pct, true, 1200);
+
+	controller->turn(-180_deg, 80_pct, false, 750);
+	controller->drive(6_in);
+	pros::delay(1000); // TODO: figure out why this is needed
+
+	// walls out
+	walls.toggle();
+
+	controller->drive({-63, 81}, 100_pct, true, 2000);
+
+	// back up a bit
+	controller->drive(10_in, 100_pct, 1500);
+
+	// walls in
+	walls.toggle();
+
+	controller->drive({-75, 56}, 100_pct, true, 2000);
+
+	// walls out
+	walls.toggle();
+
+	controller->drive({-72, 95}, 100_pct, true, 2000);
+	controller->drive(14_in);
+
+	// walls in
+	walls.toggle();
+
+	pros::delay(5000);
+
+	// drive back
+	controller->drive({-6, 87}, 80_pct);
+	controller->drive({0, 0}, 60_pct);
+
+	/*controller->turn(50_deg, 127, false, 750);
 	controller->drive(82_in, 100, 4500); // this should be 82
 	controller->turn(125_deg, 127, false, 750);
 	controller->drive(-22_in, 127, 1500);
@@ -91,27 +147,25 @@ void autonomous() {
 	controller->drive(-14_in, 127, 1500);
 	controller->turn(155_deg, 127, false, 750);
 	controller->drive(-24_in, 127, 1500);
-	controller->turn(-45_deg, 100, false, 750); // TODO: TUNE THIS!!!
+	controller->turn(-45_deg, 100, false, 750); // TODO: TUNE THIS!!!*/
 }
 
 void opcontrol() {
 	//controller->drive(LAT_MOVE);
 	//controller->turn(ANG_MOVE);
 
-	//controller->turn(20_deg);
-	//controller->drive(12_in);
-	//controller->turn(0_deg);
-
-	//controller->drive(-12);
-
-	controller->drive({0, 12});
-
 	chassis->setDriveExponent(3);
  	chassis->setControllerDeadband(12);
+
+	chassis->setArcadeDriveChannels(pros::E_CONTROLLER_ANALOG_LEFT_Y, pros::E_CONTROLLER_ANALOG_RIGHT_X);
+	chassis->setSecondaryArcadeTurnChannel(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+	//controller->drive({12, 12}, 127);
 
 	while (true) {
 		// step our tank drive loop, handled by reauto
 		chassis->tank();
+		//chassis->arcade();
 
 		// get pose
 		Pose p = chassis->getPose();
